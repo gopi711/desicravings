cdb4d7537f4d9a2974edb5fb040b877f6a67b3d5
Gopi Sairam <rsai2890@gmail.com>
2024-03-13 20:55:32 -0500

    Update requirements.txt

--- desicravings/settings.py
+++ desicravings/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, "static"),
